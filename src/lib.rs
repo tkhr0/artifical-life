dcf96ca4176d4d3ae42d018092002cb359c46c0f
KarasawaTakahiro <karasawa@basicinc.jp>
2020-03-18 19:22:36 +0900

    draw field

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,37 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Life {
+    x: u32,
+    y: u32,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, artifical-life!");
+pub struct Universe {
+    width: u32,
+    height: u32,
+    lives: Vec<Life>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 500;
+        let height = 500;
+        let lives = (0..20).map(|i| Life { x: i, y: i }).collect();
+
+        Universe {
+            width: width,
+            height: height,
+            lives: lives,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
 }
